c2e23ee35ebc0ac3b5f98bbdfcf9ffa8ee5a96ff
SHermukhammad <103446776+SHermukhammadjon@users.noreply.github.com>
2022-05-19 22:30:51 +0500

    15dars papkasidagi 3-topshriq vazifasi bajarildi!

--- dars15/3.py
+++ dars15/3.py
@@ -5,3 +5,21 @@ davlatning poytaxtini konsolga chiqaring. Agar foydalanuvchi
 lug'atda yo'q davlatni kiritsa, "Bizda bunday ma'lumot yo'q" 
 degan xabarni chiqaring.
 """
+davlat = input('Istalgan davlat kiriting ___>>>')
+
+davlatlar = {
+	'o\'zbekiston' : 'toshkent',
+	'qozog\'iston' : 'olmaota',
+	'aqsh' : 'vashingto\'ng',
+	'rossiya' : 'maskva',
+	'turkiya' : 'istambul',
+	'germanya' : 'berlin',
+	'fransya' : 'parij',
+	'britanya' : 'london',
+	'yapo\'niya' : 'tokio'
+}
+
+if davlat in davlatlar:
+	print(f'{davlat}ning poytaxti {davlatlar[davlat]}!')
+else:
+	print(f'Kechirasiz biznig ma\'lumotlar bazamizdan \'{davlat}\' afsuski topilmadi:(')
