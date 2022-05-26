91f2873598ab717c03a7db21f78663f7ef3e08b6
SHermukhammad <103446776+SHermukhammadjon@users.noreply.github.com>
2022-05-26 21:51:51 +0500

    15-dasrsning 4-dasturi tuzildi!

--- dars15/4.py
+++ dars15/4.py
@@ -7,3 +7,25 @@ solishtiring, agar taom menuda bo'lsa narhini ko'rsating, aks
 holda "bizda bunday taom yo'q" degan xabarni chiqaring
 """
 
+menyu = {
+	'osh' : 15_000,
+	'somsa' : 8_000,
+	'kabob' : 12_000,
+	'choy' : 12_00,
+	'non' : 4_000,
+	'manti' : 18_000,
+	'sho\'rva'  : 10_000,
+	'qo\'virdoq' : 20_000,
+	'mastava' : 15_000,
+	'sharbat' : 3_000
+    }
+
+for n in range(1,4):
+	taom = input(f'{n}-taom nomini kiriting_>>>')
+	taom.lower()
+	if taom in menyu:
+		print(f'{taom.capitalize()}ning narxi {menyu[taom]} so\'m \n')
+	else:
+		print(f'Bizda {taom} afsuski yo\'q :( \n')
+
+
