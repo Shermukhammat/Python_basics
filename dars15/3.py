2111e9ac5893d7cefe4ec16de0766aa274ac9e7c
SHermukhammad <103446776+SHermukhammadjon@users.noreply.github.com>
2022-05-26 21:24:55 +0500

    15-darsning 3-dasturidagi xatolar tuzatildi

--- dars15/3.py
+++ dars15/3.py
@@ -5,21 +5,29 @@ davlatning poytaxtini konsolga chiqaring. Agar foydalanuvchi
 lug'atda yo'q davlatni kiritsa, "Bizda bunday ma'lumot yo'q" 
 degan xabarni chiqaring.
 """
-davlat = input('Istalgan davlat kiriting ___>>>')
+
 
 davlatlar = {
-	'o\'zbekiston' : 'toshkent',
-	'qozog\'iston' : 'olmaota',
-	'aqsh' : 'vashingto\'ng',
-	'rossiya' : 'maskva',
-	'turkiya' : 'istambul',
-	'germanya' : 'berlin',
-	'fransya' : 'parij',
-	'britanya' : 'london',
-	'yapo\'niya' : 'tokio'
-}
+	'O\'zbekiston' : 'Toshkent',
+	'Qozog\'iston' : 'Olmaota',
+	'AQSH' : 'Vashingto\'ng',
+	'Rossiya' : 'Maskva',
+	'Turkiya' : 'Istambul',
+	'Germanya' : 'Berlin',
+	'Fransya' : 'Parij',
+	'Britanya' : 'London',
+	'Yapo\'niya' : 'To\'kio'
+    }
+
+kirish = input('Istalgan davlat kiriting ___>>>')
+
+if kirish.lower() == 'aqsh':      #agar kirish aqsh bolsa;
+	davlat = kirish.upper()       #davlat = kirish yuqori registir bn yozilganiga;
+else:                             #aks holda;
+	davlat = kirish.capitalize()  #davlat = kirishning 1-harfi yuqori registor bilan yozilganiga;
 
 if davlat in davlatlar:
+	
 	print(f'{davlat}ning poytaxti {davlatlar[davlat]}!')
 else:
 	print(f'Kechirasiz biznig ma\'lumotlar bazamizdan \'{davlat}\' afsuski topilmadi:(')
