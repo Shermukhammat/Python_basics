4d66a9541d7fadd20b1b80d427190bb50c9b58b6
SHermukhammad <103446776+SHermukhammadjon@users.noreply.github.com>
2022-06-12 11:20:59 +0500

    17-darsning 2-dasturi xatolarini tuzatdim xato tab va speyslar aralshganidan ekan! ammo Inputga ext yoki quidan boshga str malumot kiritilsa xatolik beradi bu joyini kodini ishlata olmadim hozircha!

--- dars17/2.py
+++ dars17/2.py
@@ -8,42 +8,37 @@ Foydalanuvchi exit yoki quit deb yozganda dastur to'xtasin (ikkita
 shartni ham tekshiring). Yuqoridagi dasturni turli usullarda yozib 
 ko'ring (break, ishora, yoki shart tekshirish)
 """
-toxta = True
-yosh  = ''
-summa = 0
-savol = '\nAssalomu alaykum yoshingz nechchida?>>'
+savol = 'yosh:'
+chiqsh_savoli = 'Dasturdan chiqishni xoxlaysizmi(xa\\yo\'q)?\n>>>'
+ishora = True
+yosh = 0
 
-while toxta:
+while ishora:
+    qiymat = input(savol)
+    tip = type(qiymat)
+    if qiymat == 'exit' or qiymat == 'quit':
+        xa_yoq = input(chiqsh_savoli) 
+        if xa_yoq.lower() == 'xa':
+            ishora = False
+            print('...')
+        else:
+            print('....')
+            continue
+    # if tip == "<class 'str'>":
+    #     print('....')
+    #     continue
 
-	yosh = input(savol)
-    
-    if yosh != 'exit' or 'quit':
-    	yosh2 = int(yosh)
-        
-    	if yosh2 <= 7 :
-		    summa = 2_000
-		    print(f"Sizga kirish {summa} so'm!")
-
-	    elif yosh2 <= 18:
-		    summa = 3_000
-		    print(f"Sizga kirish {summa} so'm!")
-	    
-	    elif yosh2 <= 65:
-		    summa = 10_000
-		    print(f"Sizga kirish {summa} so'm!")
-	    
-	    else:
-		    print('Sizga kirish bepul!')
-	
-	else:
-		print('xayir')
-		toxta = False
-
-#  		hohlaysizmi = input('Dasturdan chiqshni hohlaysizmi?(ha\\yo\'q)')
-
-
-	
-
-
-
-
+    else:
+        yosh = int(qiymat)
+        if yosh <= 7 :
+            narx = 2_000
+            print(f"Sizga kirish {narx} so'm!")
+        elif yosh <= 18:
+            narx = 3_000
+            print(f"Sizga kirish {narx} so'm!")
+        elif yosh <= 65:
+            narx = 10_000
+            print(f"Sizga kirish {narx} so'm!")
+        else:
+            narx = 0
+            print('Sizga kirish bepul!')
