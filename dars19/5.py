39cbce8de53dc7668efc4c21c6e6635f19e007bc
SHermukhammad <103446776+SHermukhammadjon@users.noreply.github.com>
2022-06-20 18:33:46 +0500

    19-darsni 5-dasturini tuzib tugatdim:)

---
+++ dars19/5.py
@@ -0,0 +1,15 @@
+# 5. Foydalanuvchidan x va y sonlarini olib, x^y ni konsolga chiqaruvchi 
+# funksiya yozing.Yuqoridagi funksiyada y uchun 2 standart qiymatini bering.
+
+def daraja(son = 0, darajasi = 0) :
+	""" 
+	foydalanuvchidan son va daraja parametirlarini olib natijani
+	hisoblab konsolga chiqaruvchi funksya!
+	"""
+	natija = son**darajasi
+	print(f"\n{son} ^ {darajasi} = {natija}")
+
+son = int(input('\nsonni kiriting:\n>>>'))
+son_darajasi = int(input(f'\n{son} ni darajasini kiriting:\n>>>'))
+
+daraja(son = son, darajasi = son_darajasi)
