8e81055213e7f2fd7b6583caf929d96a0ecb3b8e
SHermukhammadjon <shermukhammadtemirov@gmail.com>
2022-12-16 14:27:01 +0500

    I writtong 29nd lesson

--- dars28/main.py
+++ dars28/main.py
@@ -1,25 +1,34 @@
-#28
+#28 OBJECT ORIENTED PROGRAMMING. KLASS VA OBYEKT
+
+
+# son = 1 
+# matin = "salom"
+# print(type(son))
+# print(type(matin))
 
 class Talaba:
-	def __init__(self, ism, familya, tyil):
+	def __init__(self, ism, familya, tugilgan_yil):
 		self.name = ism
-		self.familya = familya
-		self.tyil = tyil
+		self.second_name = familya
+		self.born_year = tugilgan_yil
+
 	def tanishtir(self):
-		return f"Ismim {self.name} familyam {self.familya}"
+		return f"Mening ismim {self.name}, tug'lgan yilim {self.born_year}!"
+
 	def get_name(self):
 		return self.name
-	def get_age(self, yil):
-		return yil - self.tyil
-	def get_lastname(self):
-		return self.familya
-talaba1 = Talaba("Olim", "Olimov", 2000)
-talaba2 = Talaba("Hasan", "hasanov", 2007)
-talaba3 = Talaba("Ali", "Alimov", 2008)
+
+	def get_age(self, year):
+		return year - self.born_year
+
+talaba1 = Talaba("Olim", "Olimov", 2001)
+talaba2 = Talaba("Hasan", "Olimov", 2002)
+talaba3 = Talaba("Akrom", "Olimov", 2003)
+
+# print(talaba1.second_name)
+# print(talaba2.second_name)
+# print(talaba3.born_year)
+
 print(talaba1.tanishtir())
-print(talaba1.tyil)
-print(talaba1.get_name())
+print(talaba2.get_name())
 print(talaba3.get_age(2022))
-print(talaba1.get_lastname())
-
-
